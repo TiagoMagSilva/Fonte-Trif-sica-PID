76f91bde2ac41f4f9f2867784806edb1b2a9d643
tiago.magalhaes.memt <tiago.msilva@hotmail.com>
2022-06-21 08:50:12 -0300

    Alteração de nome de guia

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1587,7 +1587,7 @@ namespace FonteTrifasicaPID
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1008, 688);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "DigPot";
+            this.tabPage2.Text = "Configurações Manuais";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // groupBox11
